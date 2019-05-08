ad83f4dbaa7c579d8991bf9d557962191b2011e6
Guest User <Guest@epicodus-79.local>
2019-05-08 11:25:37 -0700

    2 reducers slice state, combined in tested rootreducer

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import TicketList from './TicketList';
 import TicketDetail from './TicketDetail';
+import { connect } from 'react-redux';
 
 function Admin(props){
   let optionalSelectedTicketContent = null;
-  if (props.selectedTicket != null){
+  if (props.selectedTicket.length > 0){
     optionalSelectedTicketContent =  <TicketDetail selectedTicket={props.ticketList[props.selectedTicket]}/>;
   }
   return (
     <div>
       <h2>Admin</h2>
-      <div id="highlight">{optionalSelectedTicketContent}</div>
+      {optionalSelectedTicketContent}
       <TicketList
         ticketList={props.ticketList}
-        currentRouterPath={props.currentRouterPath}
-        onTicketSelection={props.onTicketSelection}/>
+        currentRouterPath={props.currentRouterPath} />
     </div>
   );
 }
@@ -23,8 +23,14 @@ function Admin(props){
 Admin.propTypes = {
   ticketList: PropTypes.object,
   currentRouterPath: PropTypes.string.isRequired,
-  onTicketSelection: PropTypes.func.isRequired,
   selectedTicket: PropTypes.string
 };
 
-export default Admin;
+const mapStateToProps = state => {
+  return {
+    selectedTicket: state.selectedTicket,
+    ticketList: state.masterTicketList
+  };
+};
+
+export default connect(mapStateToProps)(Admin);
