63a8ee98b3eafbdd586656318e003c640477b491
Guest User <Guest@epicodus-79.local>
2019-05-08 16:29:45 -0700

    reconfiguring for firebase

--- __tests__/reducers/ticket-list-reducer.test.js
+++ __tests__/reducers/ticket-list-reducer.test.js
@@ -1,6 +1,7 @@
 import ticketListReducer from './../../src/reducers/ticket-list-reducer';
 import Moment from 'moment';
-import c from './../../src/constants';
+import constants from './../../src/constants';
+const { c } = constants;
 
 
 describe('ticketListReducer', () => {
--- src/index.jsx
+++ src/index.jsx
@@ -2,24 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import { HashRouter } from 'react-router-dom';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/index';
-import { createStore, applyMiddleware } from 'redux';
-import persistDataLocally from './middleware/persist-data-locally';
 
-
-let retrievedState;
-try {
-  retrievedState = localStorage.getItem('reduxStore');
-  if (retrievedState === null){
-    retrievedState = {};
-  }
-  retrievedState = JSON.parse(retrievedState);
-} catch (err){
-  retrievedState = {};
-}
-
-const store = createStore(rootReducer, retrievedState, applyMiddleware(persistDataLocally));
+const store = createStore(rootReducer);
 
 let unsubscribe = store.subscribe(() =>
   console.log(store.getState())
