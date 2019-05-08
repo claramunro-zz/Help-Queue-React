9794f311cf371145f614e7b9581fa326ce67572c
Guest User <Guest@epicodus-79.local>
2019-05-08 15:54:08 -0700

    middleware persisting local storage

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import { HashRouter } from 'react-router-dom';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/index';
+import { createStore, applyMiddleware } from 'redux';
+import persistDataLocally from './middleware/persist-data-locally';
 
-const store = createStore(rootReducer);
+
+let retrievedState;
+try {
+  retrievedState = localStorage.getItem('reduxStore');
+  if (retrievedState === null){
+    retrievedState = {};
+  }
+  retrievedState = JSON.parse(retrievedState);
+} catch (err){
+  retrievedState = {};
+}
+
+const store = createStore(rootReducer, retrievedState, applyMiddleware(persistDataLocally));
 
 let unsubscribe = store.subscribe(() =>
   console.log(store.getState())
